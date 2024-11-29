71e95bbf811bfed2920259f87777a78c0b60a966
Cecilia <cecilia.goncalves@sou.inteli.edu.br>
2024-11-29 11:00:39 -0300

    melhorias no código

--- src/cg/cg/navegacao_mapeada.py
+++ src/cg/cg/navegacao_mapeada.py
@@ -11,6 +11,7 @@ class NavegacaoMapeada(Node):
         super().__init__('navegacao_mapeada')
         self.cliente_movimento = self.create_client(MoveCmd, '/move_command')
         self.cliente_mapa = self.create_client(GetMap, '/get_map')
+
         self.mapa = None
         self.posicao_inicial = None
         self.posicao_alvo = None
@@ -22,10 +23,9 @@ class NavegacaoMapeada(Node):
         rclpy.spin_until_future_complete(self, futuro)
         resultado = futuro.result()
         time.sleep(0.2)
-        if not resultado:
-            self.get_logger().error("Erro ao obter resposta do serviço de movimento.")
-        return resultado
+        return resultado if resultado else self.get_logger().error("Erro ao obter resposta do serviço.")
 
+    # Obtém o mapa com /get_map
     def obter_mapa(self):
         futuro = self.cliente_mapa.call_async(GetMap.Request())
         rclpy.spin_until_future_complete(self, futuro)
@@ -37,31 +37,38 @@ class NavegacaoMapeada(Node):
 
         formato_mapa = tuple(resposta.occupancy_grid_shape)
         dados_mapa = resposta.occupancy_grid_flattened
-        self.mapa = np.array(dados_mapa, dtype=str).reshape(formato_mapa)
+        mapa_original = np.array(dados_mapa, dtype=str).reshape(formato_mapa)
 
-        pos_inicial = np.argwhere(self.mapa == 'r')
-        pos_alvo = np.argwhere(self.mapa == 't')
+        pos_inicial = np.argwhere(mapa_original == 'r')
+        pos_alvo = np.argwhere(mapa_original == 't')
 
         if len(pos_inicial) == 0 or len(pos_alvo) == 0:
             self.get_logger().warning("Posições inicial ou alvo não encontradas no mapa.")
             return False
 
-        self.posicao_inicial, self.posicao_alvo = tuple(pos_inicial[0]), tuple(pos_alvo[0])
-        self.mapa = np.where(self.mapa == 'f', 0, 1)
+        self.posicao_inicial = tuple(pos_inicial[0])
+        self.posicao_alvo = tuple(pos_alvo[0])
+
+        # Converte o mapa para valores numéricos, onde 0 é espaço livre e 1 é obstáculo
+        self.mapa = np.where(mapa_original == 'f', 0, 1)
         self.mapa[self.posicao_inicial] = 0
         self.mapa[self.posicao_alvo] = 0
+
         self.get_logger().info(f"Posição inicial: {self.posicao_inicial}, Alvo: {self.posicao_alvo}")
         return True
 
+    # Algoritmo A* para encontrar o caminho ótimo entre o início e o objetivo
     def a_star(self, inicio, objetivo):
         def heuristica_manhattan(a, b):
             return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
         conjunto_aberto = [(0, inicio)]
-        origem, g_score = {}, {inicio: 0}
+        origem = {}
+        g_score = {inicio: 0}
 
         while conjunto_aberto:
             _, atual = heapq.heappop(conjunto_aberto)
+
             if atual == objetivo:
                 caminho = []
                 while atual in origem:
@@ -75,24 +82,29 @@ class NavegacaoMapeada(Node):
 
                 custo_tentativo = g_score[atual] + 1
                 if vizinho not in g_score or custo_tentativo < g_score[vizinho]:
-                    origem[vizinho] = (atual, direcao)
                     g_score[vizinho] = custo_tentativo
-                    heapq.heappush(conjunto_aberto, (custo_tentativo + heuristica_manhattan(vizinho, objetivo), vizinho))
+                    prioridade = custo_tentativo + heuristica_manhattan(vizinho, objetivo)
+                    heapq.heappush(conjunto_aberto, (prioridade, vizinho))
+                    origem[vizinho] = (atual, direcao)
 
-        self.get_logger().warning("Nenhum caminho encontrado.")
+        self.get_logger().warning("Nenhum caminho disponível. Encerrando navegação.")
         return []
 
     def obter_vizinhos(self, posicao):
         direcoes = {
-            'cima': (-1, 0), 'baixo': (1, 0),
-            'esquerda': (0, -1), 'direita': (0, 1)
+            'up': (-1, 0),
+            'down': (1, 0),
+            'left': (0, -1),
+            'right': (0, 1)
         }
-        return [
-            (direcao, (posicao[0] + di, posicao[1] + dj))
-            for direcao, (di, dj) in direcoes.items()
-            if 0 <= posicao[0] + di < self.mapa.shape[0] and 0 <= posicao[1] + dj < self.mapa.shape[1]
-        ]
+        vizinhos = []
+        for direcao, (di, dj) in direcoes.items():
+            novo_i, novo_j = posicao[0] + di, posicao[1] + dj
+            if 0 <= novo_i < self.mapa.shape[0] and 0 <= novo_j < self.mapa.shape[1]:
+                vizinhos.append((direcao, (novo_i, novo_j)))
+        return vizinhos
 
+    # Lógica da navegação mapeada
     def movimento(self):
         if not self.obter_mapa():
             self.get_logger().warning("Abortando navegação devido à falha ao obter o mapa.")
@@ -100,7 +112,7 @@ class NavegacaoMapeada(Node):
 
         caminho = self.a_star(self.posicao_inicial, self.posicao_alvo)
         if not caminho:
-            self.get_logger().warning("Nenhum caminho planejado. Abortando navegação.")
+            self.get_logger().warning("Nenhum caminho disponível. Encerrando navegação.")
             return
 
         self.get_logger().info(f"Caminho planejado: {caminho}")
@@ -108,9 +120,9 @@ class NavegacaoMapeada(Node):
         for direcao in caminho:
             resposta = self.requisitar_movimento(direcao)
             if resposta and resposta.success:
-                self.get_logger().info(f'Movimento bem-sucedido para {direcao}. Nova posição: {resposta.robot_pos}')
+                self.get_logger().info(f"Movimento bem-sucedido para {direcao}. Nova posição: {resposta.robot_pos}")
             else:
-                self.get_logger().warning("Falha ao mover o robô.")
+                self.get_logger().warning(f"Falha ao mover o robô para {direcao}. Encerrando navegação.")
                 break
 
 def main():
--- src/cg/cg/navegacao_reativa.py
+++ src/cg/cg/navegacao_reativa.py
@@ -14,36 +14,36 @@ class NavegacaoReativa(Node):
         self.ultima_direcao = None
 
     def requisicao_movimento(self, direction):
-        request = MoveCmd.Request()
-        request.direction = direction
-        future = self.move_client.call_async(request)
-        rclpy.spin_until_future_complete(self, future)
-        result = future.result()
+        requisicao = MoveCmd.Request()
+        requisicao.direction = direction
+        futuro = self.move_client.call_async(requisicao)
+        rclpy.spin_until_future_complete(self, futuro)
+        resultado = futuro.result()
         time.sleep(0.1)
-        return result if result else self.get_logger().error("Erro ao obter resposta do serviço.")
-    
+        return resultado if resultado else self.get_logger().error("Erro ao obter resposta do serviço.")
+
     # Lógica da navegação reativa
     def movimento(self):
         while rclpy.ok():
-            response = self.requisicao_movimento('')
-            if not response:
+            resposta = self.requisicao_movimento('')
+            if not resposta:
                 break
 
-            robot_pos = tuple(response.robot_pos)
-            target_pos = tuple(response.target_pos)
-            sensors = {'left': response.left, 'down': response.down, 'up': response.up, 'right': response.right}
+            robot_pos = tuple(resposta.robot_pos)
+            target_pos = tuple(resposta.target_pos)
+            sensors = {'left': resposta.left, 'down': resposta.down, 'up': resposta.up, 'right': resposta.right}
 
             self.posicao_visitada.add(robot_pos)
 
             # Mover na direção do alvo (Follow-the-Target)
-            move_direction = self.encontrar_direcao(robot_pos, target_pos, sensors)
+            direcao_movimento = self.encontrar_direcao(robot_pos, target_pos, sensors)
 
             # Backtracking (se necessário)
-            if move_direction:
-                move_result = self.requisicao_movimento(move_direction)
-                if move_result.success:
-                    self.get_logger().info(f'Movimento bem-sucedido para {move_direction}. Nova posição: {move_result.robot_pos}')
-                    self.ultima_direcao = move_direction
+            if direcao_movimento:
+                resultado_movimento = self.requisicao_movimento(direcao_movimento)
+                if resultado_movimento.success:
+                    self.get_logger().info(f'Movimento bem-sucedido para {direcao_movimento}. Nova posição: {resultado_movimento.robot_pos}')
+                    self.ultima_direcao = direcao_movimento
                     if sum(1 for v in sensors.values() if v in ['f', 't']) > 1:
                         self.pilha_exploracao.append(robot_pos)
                 continue
@@ -58,35 +58,35 @@ class NavegacaoReativa(Node):
     # Define a melhor direção com base no target e/ou acompanhando os obstaculos
     def encontrar_direcao(self, robot_pos, target_pos, sensors):
         directions = ['left', 'down', 'up', 'right']
-        wall_follow_order = ['right', 'down', 'left', 'up']
-        min_distance = float('inf')
-        best_direction = None
+        ordem_paredes = ['right', 'down', 'left', 'up']
+        distancia_minima = float('inf')
+        melhor_direcao = None
 
         # 1 opção: se aproximar do alvo
         for direction, adjacent in zip(directions, [sensors[d] for d in directions]):
             if adjacent in ['f', 't']:
-                new_pos = self.simular_movimento(robot_pos, direction)
-                if tuple(new_pos) not in self.posicao_visitada:
-                    distance = abs(new_pos[0] - target_pos[0]) + abs(new_pos[1] - target_pos[1])
-                    if distance < min_distance:
-                        min_distance = distance
-                        best_direction = direction
+                nova_pos = self.simular_movimento(robot_pos, direction)
+                if tuple(nova_pos) not in self.posicao_visitada:
+                    distance = abs(nova_pos[0] - target_pos[0]) + abs(nova_pos[1] - target_pos[1])
+                    if distance < distancia_minima:
+                        distancia_minima = distance
+                        melhor_direcao = direction
 
         # 2 opção: contornar obstáculos (Boundary Following)
-        if not best_direction:
-            for direction in wall_follow_order:
+        if not melhor_direcao:
+            for direction in ordem_paredes:
                 if sensors[direction] in ['f', 't'] and direction != self.direcao_oposta(self.ultima_direcao):
                     return direction
 
-        return best_direction
+        return melhor_direcao
 
     def simular_movimento(self, pos, direction):
         offsets = {'left': (0, -1), 'down': (1, 0), 'up': (-1, 0), 'right': (0, 1)}
         return [pos[0] + offsets[direction][0], pos[1] + offsets[direction][1]]
 
     def direcao_oposta(self, direction):
-        opposites = {'left': 'right', 'right': 'left', 'up': 'down', 'down': 'up'}
-        return opposites.get(direction)
+        opostos = {'left': 'right', 'right': 'left', 'up': 'down', 'down': 'up'}
+        return opostos.get(direction)
 
     # Direção para se mover em direção à posição de backtracking
     def mover(self, target_pos):
@@ -110,8 +110,8 @@ class NavegacaoReativa(Node):
 
 def main():
     rclpy.init()
-    navigator = NavegacaoReativa()
-    navigator.movimento()
+    navegacao_reativa = NavegacaoReativa()
+    navegacao_reativa.movimento()
     rclpy.shutdown()
 
 if __name__ == '__main__':
